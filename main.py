f6f49d68c6d766ad49c96f79859f4c063eb2c765
Phineas2333 <40332459+Phineas2333@users.noreply.github.com>
2024-12-28 23:27:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def run():
         messagetxt = "未登录状态无法进行后续操作, 请检查Cookie有效性"
     try:
         server_key = get_secrets("SERVERPUSHKEY")
-        if not (messagetxt == "荧光棒捐赠结束")
+        if not (messagetxt == "荧光棒捐赠结束"):
             send_message(server_key, messagetxt)
     except Exception as e:
         logger.info("当前未配置Server酱推送，任务结束")
